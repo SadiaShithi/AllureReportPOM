3a8bf1d15b20b43c481febf533f19da77741dd39
sadiashithi <shithi.20@gmail.com>
2022-07-20 04:42:00 +0600

    add method for most expensive item

--- src/test/java/pages/Landing_Page.java
+++ src/test/java/pages/Landing_Page.java
@@ -36,14 +36,45 @@ public class Landing_Page extends Base {
 	}
 
 	public void makeList() {
-		List<WebElement> elements = driver.findElements(items);
+		List<WebElement> products = driver.findElements(items);
+		List<WebElement> productPrices = driver.findElements(itemPrice);
 
-		for (WebElement item : elements) {
-			System.out.println(item.getText());
+		try {
+			if (products.size() == productPrices.size()) {
+				System.out.println("Making A List of Products Below...");
+			}
+		} catch (ArithmeticException e) {
+			System.out.println("Please check the xpaths again.");
+		}
+		System.out.println("\nS/N			Product Names				Product's Price");
+		for (int i = 0; i < products.size(); i++) {
+			System.out.println(i + 1 + ". " + products.get(i).getText() + "	-	" + productPrices.get(i).getText());
 		}
 	}
 
 	public void expensiveItem() {
+		List<WebElement> productPrices = driver.findElements(itemPrice);
+		int f = 0;
+
+		for (WebElement price : productPrices) {
+			String s = price.getText();
+			char r = s.charAt(0);
+			String S = s.replace(r, ' ').trim();
+			String num = S.replace(",", "").trim();
+			int n = Integer.parseInt(num);
+
+			// System.out.println(n);
+
+			List<Integer> numbers = new ArrayList<Integer>(n);
+			Collections.sort(numbers);
+
+			// System.out.println(numbers);
+
+			f = numbers.get(numbers.size()- 1);
+
+		}
+		System.out.print("The most expensive product in the list costs $" + f);
+
 		 
 	}
 
