e90c1992e09926da04e8cc0438dacf30e53a8526
sadiashithi <shithi.20@gmail.com>
2022-07-20 04:09:33 +0600

    add with bugs

--- src/test/java/pages/Landing_Page.java
+++ src/test/java/pages/Landing_Page.java
@@ -1,7 +1,8 @@
 package pages;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -28,23 +29,53 @@ public class Landing_Page extends Base {
 		driver.findElement(hitIcon).click();
 	}
 
-	public void slideMe() {
+	public void slideMe(int px) {
 		WebElement slideMe = driver.findElement(slider);
 
 		Actions action = new Actions(driver);
-		action.dragAndDropBy(slideMe, -75, 0).perform();
+		action.dragAndDropBy(slideMe, px, 0).perform();
 	}
 
 	public void makeList() {
-		List<WebElement> elements = driver.findElements(items);
+		List<WebElement> products = driver.findElements(items);
+		List<WebElement> productPrices = driver.findElements(itemPrice);
 
-		for (WebElement item : elements) {
-			System.out.println(item.getText());
+		try {
+			if (products.size() == productPrices.size()) {
+				System.out.println("Making A List of Products Below...");
+			}
+		} catch (ArithmeticException e) {
+			System.out.println("Please check the xpaths again.");
+		}
+		System.out.println("\nS/N			Product Names				Product's Price");
+		for (int i = 0; i < products.size(); i++) {
+			System.out.println(i + 1 + ". " + products.get(i).getText() + "	-	" + productPrices.get(i).getText());
 		}
 	}
 
 	public void expensiveItem() {
-		 
+		List<WebElement> productPrices = driver.findElements(itemPrice);
+		int f = 0;
+
+		for (WebElement price : productPrices) {
+			String s = price.getText();
+			char r = s.charAt(0);
+			String S = s.replace(r, ' ').trim();
+			String num = S.replace(",", "").trim();
+			int n = Integer.parseInt(num);
+
+			// System.out.println(n);
+
+			List<Integer> numbers = new ArrayList<Integer>(n);
+			Collections.sort(numbers);
+
+			// System.out.println(numbers);
+
+			f = numbers.get(numbers.size()- 1).getInteger(num);
+
+		}
+		System.out.print("The most expensive product in the list costs $" + f);
+
 	}
 
 }
--- src/test/java/testCases/List_and_Get_the_Expensive_Item_Test.java
+++ src/test/java/testCases/List_and_Get_the_Expensive_Item_Test.java
@@ -1,14 +1,15 @@
 package testCases;
+
 import base.Base;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import pages.Landing_Page;
 
-public class Sort_TV_and_List_Item_Test extends Base {
-	
+public class List_and_Get_the_Expensive_Item_Test extends Base {
+
 	Landing_Page landingPage = new Landing_Page();
-	
+
 	@Given("I am redirecting to {string}")
 	public void i_am_redirecting_to(String string) {
 		getURL(string);
@@ -27,19 +28,20 @@ public class Sort_TV_and_List_Item_Test extends Base {
 	}
 
 	@Then("Sort Item by slider {int} px")
-	public void sort_item_by_slider_px(Integer int1) {
-	    landingPage.slideMe();
+	public void sort_item_by_slider_px(Integer px) throws InterruptedException {
+		landingPage.slideMe(px);
+		Thread.sleep(5000);
 	}
 
 	@Then("Make a List of Products on the Page")
 	public void make_a_list_of_products_on_the_page() {
-	   landingPage.makeList();
+		landingPage.makeList();
 	}
 
 	@Then("Find the Most Expensive Item Among Those")
 	public void find_the_most_expensive_item_among_those() throws InterruptedException {
-	    landingPage.expensiveItem();
-	    Thread.sleep(5000);
+		landingPage.expensiveItem();
+		Thread.sleep(5000);
 	}
-	
+
 }
